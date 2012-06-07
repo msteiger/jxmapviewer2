437a46d77ef3e76e5570c5a31dcc72ad75037ba6
Martin Steiger <git@martin-steiger.de>
2012-06-07 13:04:20 +0200

    Added route-painting example

---
+++ examples/src/sample2/RoutePainter.java
@@ -0,0 +1,94 @@
+
+package sample2;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdesktop.swingx.JXMapViewer;
+import org.jdesktop.swingx.mapviewer.GeoPosition;
+import org.jdesktop.swingx.painter.Painter;
+
+/**
+ * Paints a route
+ * @author Martin Steiger
+ */
+public class RoutePainter implements Painter<JXMapViewer>
+{
+	private Color color = Color.RED;
+	private boolean antiAlias = true;
+	
+	private List<GeoPosition> track;
+	
+	/**
+	 * @param track the track
+	 */
+	public RoutePainter(List<GeoPosition> track)
+	{
+		// copy the list so that changes in the 
+		// original list do not have an effect here
+		this.track = new ArrayList<GeoPosition>(track);
+	}
+
+	@Override
+	public void paint(Graphics2D g, JXMapViewer map, int w, int h)
+	{
+		g = (Graphics2D) g.create();
+
+		// convert from viewport to world bitmap
+		Rectangle rect = map.getViewportBounds();
+		g.translate(-rect.x, -rect.y);
+
+		if (antiAlias)
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		// do the drawing
+		g.setColor(Color.BLACK);
+		g.setStroke(new BasicStroke(4));
+
+		drawRoute(g, map);
+
+		// do the drawing again
+		g.setColor(color);
+		g.setStroke(new BasicStroke(2));
+
+		drawRoute(g, map);
+
+		g.dispose();
+	}
+
+	/**
+	 * @param g the graphics object
+	 * @param map the map
+	 */
+	private void drawRoute(Graphics2D g, JXMapViewer map)
+	{
+		int lastX = 0;
+		int lastY = 0;
+		
+		boolean first = true;
+		
+		for (GeoPosition gp : track)
+		{
+			// convert geo-coordinate to world bitmap pixel
+			Point2D pt = map.getTileFactory().geoToPixel(gp, map.getZoom());
+
+			if (first)
+			{
+				first = false;
+			}
+			else
+			{
+				g.drawLine(lastX, lastY, (int) pt.getX(), (int) pt.getY());
+			}
+			
+			lastX = (int) pt.getX();
+			lastY = (int) pt.getY();
+		}
+	}
+}
---
+++ examples/src/sample2/Sample2.java
@@ -0,0 +1,72 @@
+package sample2;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import javax.swing.JFrame;
+
+import org.jdesktop.swingx.JXMapViewer;
+import org.jdesktop.swingx.OSMTileFactoryInfo;
+import org.jdesktop.swingx.mapviewer.DefaultTileFactory;
+import org.jdesktop.swingx.mapviewer.GeoPosition;
+import org.jdesktop.swingx.mapviewer.TileFactoryInfo;
+import org.jdesktop.swingx.mapviewer.Waypoint;
+import org.jdesktop.swingx.mapviewer.WaypointPainter;
+import org.jdesktop.swingx.painter.CompoundPainter;
+
+/**
+ * A simple sample application that shows
+ * a OSM map of Europe
+ * @author Martin Steiger
+ */
+public class Sample2
+{
+	/**
+	 * @param args the program args (ignored)
+	 */
+	public static <T> void main(String[] args)
+	{
+		JXMapViewer mapViewer = new JXMapViewer();
+
+		// Create a TileFactoryInfo for OpenStreetMap
+		TileFactoryInfo info = new OSMTileFactoryInfo();
+		DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+		tileFactory.setThreadPoolSize(8);
+		mapViewer.setTileFactory(tileFactory);
+
+		GeoPosition frankfurt = new GeoPosition(50,  7, 0, 8, 41, 0);
+		GeoPosition wiesbaden = new GeoPosition(50,  5, 0, 8, 14, 0);
+		GeoPosition mainz     = new GeoPosition(50,  0, 0, 8, 16, 0);
+		GeoPosition darmstadt = new GeoPosition(49, 52, 0, 8, 39, 0);
+		GeoPosition offenbach = new GeoPosition(50,  6, 0, 8, 46, 0);
+
+		// Set the focus
+		mapViewer.setZoom(7);
+		mapViewer.setAddressLocation(frankfurt);
+		
+		List<GeoPosition> track = Arrays.asList(frankfurt, wiesbaden, mainz, darmstadt, offenbach);
+		RoutePainter routePainter = new RoutePainter(track);
+		
+		Collection<Waypoint> waypoints = Arrays.asList(
+				new Waypoint(frankfurt),
+				new Waypoint(wiesbaden),
+				new Waypoint(mainz),
+				new Waypoint(darmstadt),
+				new Waypoint(offenbach));
+
+		WaypointPainter waypointPainter = new WaypointPainter();
+		waypointPainter.setWaypoints(waypoints);
+		
+		CompoundPainter<JXMapViewer> painter = new CompoundPainter<JXMapViewer>();
+		painter.setPainters(Arrays.asList(routePainter, waypointPainter));
+		mapViewer.setOverlayPainter(painter);
+	
+		// Display the viewer in a JFrame
+		JFrame frame = new JFrame("JXMapviewer2 Example");
+		frame.getContentPane().add(mapViewer);
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
