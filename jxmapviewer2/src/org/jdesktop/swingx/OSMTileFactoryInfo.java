57f75540881859956722f8428b89c1d057be041c
Martin Steiger <git@martin-steiger.de>
2013-11-21 21:16:31 +0100

    Removed invalid file headers
    Fixed #17

--- jxmapviewer2/src/org/jdesktop/swingx/OSMTileFactoryInfo.java
+++ jxmapviewer2/src/org/jdesktop/swingx/OSMTileFactoryInfo.java
@@ -1,11 +1,3 @@
-// Fraunhofer Institute for Computer Graphics Research (IGD)
-// Department Information Visualization and Visual Analytics
-//
-// Copyright (c) Fraunhofer IGD. All rights reserved.
-//
-// This source code is property of the Fraunhofer IGD and underlies
-// copyright restrictions. It may only be used with explicit
-// permission from the respective owner.
 
 package org.jdesktop.swingx;
 
