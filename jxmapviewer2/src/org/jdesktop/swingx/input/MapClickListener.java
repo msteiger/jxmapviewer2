6b016e0b2a4e032226fd80560233a428ffa057f3
Martin Steiger <git@martin-steiger.de>
2014-04-26 14:28:15 +0200

    Cleaned up MapClickListener

--- jxmapviewer2/src/org/jdesktop/swingx/input/MapClickListener.java
+++ jxmapviewer2/src/org/jdesktop/swingx/input/MapClickListener.java
@@ -1,9 +1,10 @@
-package org.jdesktop.swingx.imput;
 
+package org.jdesktop.swingx.input;
+
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.geom.Point2D;
 
 import javax.swing.SwingUtilities;
 
@@ -22,44 +23,38 @@ public abstract class MapClickListener extends MouseAdapter {
     /**
      * Creates a mouse listener for the jxmapviewer which returns the
      * GeoPosition of the the point where the mouse was clicked.
-     *
+     * 
      * @param viewer the jxmapviewer
      */
-    public MapClickListener( JXMapViewer viewer ) {
+    public MapClickListener(JXMapViewer viewer) {
         this.viewer = viewer;
     }
 
     /**
      * Gets called on mouseClicked events, calculates the GeoPosition and fires
      * the mapClicked method that the extending class needs to implement.
-     *
-     * @param evt
+     * 
+     * @param evt the mouse event
      */
     @Override
-    public void mouseClicked( MouseEvent evt ) {
-        final boolean left = SwingUtilities.isLeftMouseButton( evt );
-        final boolean singleClick = ( evt.getClickCount() == 1 );
+    public void mouseClicked(MouseEvent evt) {
+        final boolean left = SwingUtilities.isLeftMouseButton(evt);
+        final boolean singleClick = (evt.getClickCount() == 1);
 
-        if ( ( left && singleClick ) ) {
+        if ((left && singleClick)) {
             Rectangle bounds = viewer.getViewportBounds();
-            double x = bounds.getX() + evt.getX();
-            double y = bounds.getY() + evt.getY();
-            Point2D pixelCoordinates = new Point2D.Double( x, y );
-            mapClicked( viewer.getTileFactory().pixelToGeo( pixelCoordinates, viewer.getZoom() ) );
+            int x = bounds.x + evt.getX();
+            int y = bounds.y + evt.getY();
+            Point pixelCoordinates = new Point(x, y);
+            mapClicked(viewer.getTileFactory().pixelToGeo(pixelCoordinates, viewer.getZoom()));
         }
     }
 
     /**
      * This method needs to be implemented in the extending class to handle the
-     * map Clicked event.<p>
-     *
-     * For example you can print the coordinates like this:
-     * <p>
-     *
-     * {@code System.out.println( String.format("The latitude is: %f  longitude is: %f", location.getLatitude(), location.getLongitude() ) );
-     * }
-     *
-     * @param location The GeoPosition of the click event
+     * map clicked event.
+     * 
+     * @param location The {@link GeoPosition} of the click event
      */
-    public abstract void mapClicked( GeoPosition location );
+    public abstract void mapClicked(GeoPosition location);
 }
