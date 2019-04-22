37a747a9ba727a2dbdfec50eb7529ae82bfa27c4
Martin Steiger <git@martin-steiger.de>
2019-04-22 10:59:23 +0200

    Correct OSM zoom levels - fixes #73

--- jxmapviewer2/src/main/java/org/jxmapviewer/OSMTileFactoryInfo.java
+++ jxmapviewer2/src/main/java/org/jxmapviewer/OSMTileFactoryInfo.java
@@ -5,11 +5,10 @@ import org.jxmapviewer.viewer.TileFactoryInfo;
 
 /**
  * Uses OpenStreetMap
- * @author Martin Dummer
  */
 public class OSMTileFactoryInfo extends TileFactoryInfo
 {
-    private static final int max = 19;
+    private static final int MAX_ZOOM = 19;
 
     /**
      * Default constructor
@@ -19,10 +18,14 @@ public class OSMTileFactoryInfo extends TileFactoryInfo
         this("OpenStreetMap", "http://tile.openstreetmap.org");
     }
 
+    /**
+     * @param name the name of the factory
+     * @param baseURL the base URL to load tiles from
+     */
     public OSMTileFactoryInfo(String name, String baseURL)
     {
-        super(name, 
-                1, max - 2, max, 
+        super(name,
+                0, MAX_ZOOM, MAX_ZOOM,
                 256, true, true,                     // tile size is 256 and x/y orientation is normal
                 baseURL,
                 "x", "y", "z");                        // 5/15/10.png
@@ -31,8 +34,8 @@ public class OSMTileFactoryInfo extends TileFactoryInfo
     @Override
     public String getTileUrl(int x, int y, int zoom)
     {
-        zoom = max - zoom;
-        String url = this.baseURL + "/" + zoom + "/" + x + "/" + y + ".png";
+        int invZoom = MAX_ZOOM - zoom;
+        String url = this.baseURL + "/" + invZoom + "/" + x + "/" + y + ".png";
         return url;
     }
 
