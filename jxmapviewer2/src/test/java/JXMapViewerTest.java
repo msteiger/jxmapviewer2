ac21e1ac3fbce1be2765d817efbc135cec42edeb
Peter Jochum <peter.jochum@aau.at>
2021-01-17 14:30:14 +0100

    Additional tests for JXMapViewer

--- jxmapviewer2/src/test/java/JXMapViewerTest.java
+++ jxmapviewer2/src/test/java/JXMapViewerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.geom.Point2D;
+import java.util.HashSet;
+import java.util.Set;
 
 public class JXMapViewerTest {
 
@@ -15,7 +17,7 @@ public class JXMapViewerTest {
 
 
     @BeforeEach
-    public void setup() {
+    public void setupBeforeEach() {
         mapViewer = new JXMapViewer();
 
         // Create a TileFactoryInfo for OpenStreetMap
@@ -37,4 +39,47 @@ public class JXMapViewerTest {
         Assertions.assertEquals(88780.01215942061, p.getY());
     }
 
+    @Test
+    public void setDesignTimeTest() {
+        Assertions.assertFalse(mapViewer.isDesignTime());
+        mapViewer.setDesignTime(true);
+        Assertions.assertTrue(mapViewer.isDesignTime());
+    }
+
+    @Test
+    public void setZoomTest() {
+        Assertions.assertEquals(7, mapViewer.getZoom());
+        mapViewer.setZoom(6);
+        Assertions.assertEquals(6, mapViewer.getZoom());
+    }
+
+    @Test
+    public void zoomToBestFitTest() {
+        // zoom out
+        Set<GeoPosition> postitions = new HashSet<>();
+        GeoPosition topRight = new GeoPosition(46.6485, 14.3782);
+        postitions.add(topRight);
+        GeoPosition bottomLeft = new GeoPosition(46.5938, 14.2464);
+        postitions.add(bottomLeft);
+        mapViewer.zoomToBestFit(postitions, 0.1);
+
+        // map should have zoomed in on Klagenfurt
+        // TODO: zoom stays at initial value
+        Assertions.assertEquals(7, mapViewer.getZoom());
+        GeoPosition gp = mapViewer.getCenterPosition();
+        Assertions.assertTrue(gp.getLatitude() > bottomLeft.getLatitude());
+        Assertions.assertTrue(gp.getLongitude() > bottomLeft.getLongitude());
+        Assertions.assertTrue(gp.getLatitude() < topRight.getLatitude());
+        Assertions.assertTrue(gp.getLongitude() < topRight.getLongitude());
+    }
+
+    @Test
+    public void panningEnabledTest() {
+        // Initially panning is enabled
+        Assertions.assertTrue(mapViewer.isPanningEnabled());
+
+        // Disable panning
+        mapViewer.setPanEnabled(false);
+        Assertions.assertFalse(mapViewer.isPanningEnabled());
+    }
 }
